00ac7ca69feb8a07ed4a912a6597859c40d60cc7
Ushna-FS <ushnafaheem616@gmail.com>
2025-02-28 12:01:00 +0500

    Added all colors that will be used in our App theme

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mainColor = Color(0xFFFF715B); 
+  static const Color secondaryColor = Color(0xFFECECEC);
+  static const Color notificationColor = Color.fromARGB(0, 20,13,79);
+  static const Color textColor=Colors.white;
+
+}
