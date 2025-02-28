92e6b9944171ceeafdbf82da7e5578361decea55
Ushna-FS <ushnafaheem616@gmail.com>
2025-02-28 12:02:46 +0500

    Custom fonts file

---
+++ lib/constants/app_fontStyles.dart
@@ -0,0 +1,13 @@
+import 'package:fixibot_app/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppFonts {
+  static TextStyle montserratBold60 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w700,
+    fontSize: 60,
+    height: 73.14 / 60,
+    color: AppColors.textColor,
+  );
+
+}
