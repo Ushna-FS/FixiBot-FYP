c26500f555c39980e628c1abce4aa94eaa555a65
Ushna-FS <ushnafaheem616@gmail.com>
2025-03-14 17:44:44 +0500

    Updated navigations to  profile options

--- lib/screens/profile/view/profile.dart
+++ lib/screens/profile/view/profile.dart
@@ -1,8 +1,12 @@
+
+
+import 'package:fixibot_app/screens/MyVehicles.dart';
+import 'package:fixibot_app/screens/help/support.dart';
+import 'package:fixibot_app/screens/profile/editProfile.dart';
 import '../../../constants/app_colors.dart';
 import '../../auth/view/login.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../../constants/app_fontStyles.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -17,160 +21,102 @@ class ProfileScreen extends StatelessWidget {
         padding: const EdgeInsets.only(top: 24),
         child: Column(
           children: [
-            SizedBox(
-              height: screenSize.height * 0.15,
-            ),
+            SizedBox(height: screenSize.height * 0.15),
             Padding(
-              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       CircleAvatar(
-                          backgroundColor: AppColors.secondaryColor,
-                          radius: 30,
-                          child: Image.asset(
-                            "assets/icons/profileImg.png",
-                            height: 300,
-                            width: 300,
-                          )),
+                        backgroundColor: AppColors.secondaryColor,
+                        radius: 30,
+                        child: Image.asset(
+                          "assets/icons/profileImg.png",
+                          height: 300,
+                          width: 300,
+                        ),
+                      ),
                       Padding(
                         padding: const EdgeInsets.only(left: 8.0),
                         child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              "Jasmine Rose",
-                              style: AppFonts.montserratHeading,
-                            ),
-                            Text(
-                              "email@email.com",
-                              style: AppFonts.montserratText,
-                            )
+                            Text("Jasmine Rose", style: AppFonts.montserratHeading),
+                            Text("email@email.com", style: AppFonts.montserratText),
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
                   GestureDetector(
-                      onTap: () {
-                        //Back Button
-                        Get.back();
-                      },
-                      child: Image.asset("assets/icons/backWhiteArrow.png"))
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: Image.asset("assets/icons/backWhiteArrow.png"),
+                  ),
                 ],
               ),
             ),
-            SizedBox(
-              height: screenSize.height * 0.1478,
-            ),
-            Stack(
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                      color: AppColors.textColor,
-                      borderRadius:
-                          BorderRadius.only(topRight: Radius.circular(250))),
-                  width: screenSize.width * 1,
-                  height: screenSize.height * 0.595,
+            SizedBox(height: screenSize.height * 0.1478),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: AppColors.textColor,
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(250)),
                 ),
-                Positioned(
-                    top: 100,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          GestureDetector(
-                            onTap: () {},
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset("assets/icons/profile.png"),
-                                SizedBox(
-                                  width: screenSize.width * 0.01,
-                                ),
-                                Text(
-                                  "My Vehicles",
-                                  style: AppFonts.montserratText2,
-                                )
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: screenSize.height * 0.02,
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset("assets/icons/editText.png"),
-                                SizedBox(
-                                  width: screenSize.width * 0.01,
-                                ),
-                                Text(
-                                  "Edit Profile",
-                                  style: AppFonts.montserratText2,
-                                )
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: screenSize.height * 0.02,
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset("assets/icons/help.png"),
-                                SizedBox(
-                                  width: screenSize.width * 0.01,
-                                ),
-                                Text(
-                                  "Help & Support",
-                                  style: AppFonts.montserratText2,
-                                )
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: screenSize.height * 0.01,
-                          ),
-                        ],
-                      ),
-                    )),
-                Positioned(
-                    bottom: 40,
-                    right: 20,
-                    child: GestureDetector(
-                      onTap: () {
-                        // LogOut Button
-                        Get.to(const Login());
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Logout",
-                            style: AppFonts.montserratText2,
-                          ),
-                          SizedBox(
-                            width: screenSize.width * 0.01,
-                          ),
-                          Image.asset("assets/icons/logout.png"),
-                        ],
-                      ),
-                    ))
-              ],
+                width: double.infinity,
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      leading: Image.asset("assets/icons/profile.png"),
+                      title: InkWell(
+    onTap: () => Get.to(MyVehicleScreen()),
+    child: Text("My Vehicles", style: AppFonts.montserratText2),
+  ),
+                    ),
+                   ListTile(
+  leading: Image.asset("assets/icons/editText.png"),
+  title: InkWell(
+    onTap: () => Get.to(EditProfile()),
+    child: Text("Edit Profile", style: AppFonts.montserratText2),
+  ),
+),
+ListTile(
+  leading: Image.asset("assets/icons/help.png"),
+  title: InkWell(
+    onTap: () => Get.to(HelpSupportPage()),
+    child: Text("Help & Support", style: AppFonts.montserratText2),
+  ),
+),
+                    const Spacer(),
+                    
+Align(
+  alignment: Alignment.bottomRight,
+  child: Padding(
+    padding: const EdgeInsets.only(bottom: 20.0, right: 20.0), // Adjust padding as needed
+    child: GestureDetector(
+      onTap: () => Get.to(const Login()),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assets/icons/logout.png"),
+          const SizedBox(width: 8), 
+          Text("Logout", style: AppFonts.montserratText2),
+        ],
+      ),
+    ),
+  ),
+),
+
+
+                  ],
+                ),
+              ),
             ),
           ],
         ),
