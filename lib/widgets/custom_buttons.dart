152c76c567e8050eaf7592f20d30599c3f8d78a8
Ushna-FS <ushnafaheem616@gmail.com>
2025-03-14 12:28:59 +0500

    Added custom btns for homepage

--- lib/widgets/custom_buttons.dart
+++ lib/widgets/custom_buttons.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../constants/app_colors.dart';
+import 'package:fixibot_app/constants/app_colors.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
@@ -68,4 +68,73 @@ class CustomButton extends StatelessWidget {
       ],
     );
   }
+}
+
+class CustomHomeButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color color;
+  final Color textColor;
+  final bool isOutlined;
+  final Widget? icon;
+
+  const CustomHomeButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.color = const Color(0xFFFFAFA4), 
+    this.textColor = Colors.white,
+    this.isOutlined = false,
+    this.icon,
+  }) : super(key: key);
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 175,
+      height: 35,
+      child: isOutlined
+          ? OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                side: BorderSide(color: color, width: 1.5),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(2),
+                ),
+              ),
+              onPressed: onPressed,
+              child: _buildContent(),
+            )
+          : ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: color,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: onPressed,
+              child: _buildContent(),
+            ),
+    );
+  }
+
+  Widget _buildContent() {
+    return Row(
+      mainAxisSize: MainAxisSize.min, 
+      children: [
+        Text(
+          text,
+          style: TextStyle(fontSize: 14, color: textColor, fontWeight: FontWeight.bold),
+        ),
+        if (icon != null) ...[
+          SizedBox(width: 5), 
+          icon!, 
+        ],
+      ],
+    );
+  }
+
+
 }
