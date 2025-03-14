6bccbbfb4a6a40bb728cd660e2f71651459cf395
Ushna-FS <ushnafaheem616@gmail.com>
2025-03-14 22:24:31 +0500

    Added general vehicle page

---
+++ lib/screens/MyVehicles.dart
@@ -0,0 +1,38 @@
+import 'package:fixibot_app/constants/app_colors.dart';
+import 'package:fixibot_app/constants/app_fontStyles.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MyVehicleScreen extends StatefulWidget {
+  const MyVehicleScreen({super.key});
+
+  @override
+  State<MyVehicleScreen> createState() => _MyVehicleScreenState();
+}
+
+class _MyVehicleScreenState extends State<MyVehicleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+    
+        elevation: 1,
+        title: Text("Vehicles Information", style: AppFonts.montserrathomecardText),
+        centerTitle: true,
+        leading: IconButton(
+                    onPressed: () {
+                      Get.back();
+                    }, 
+                    icon: Image.asset('assets/icons/back.png',
+                    width: 30,
+                    height:30),
+                    ),
+      
+      ),
+      backgroundColor: AppColors.secondaryColor,
+      body: Container(
+        child: Text('My vehicles'),
+      ),
+    );
+  }
+}
