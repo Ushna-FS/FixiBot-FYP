de35d79ab97c9930ef104dc5fa511a789b5de6fb
Ushna-FS <ushnafaheem616@gmail.com>
2025-05-16 00:01:44 +0500

    Changed background vector images

--- lib/screens/auth/view/signup.dart
+++ lib/screens/auth/view/signup.dart
@@ -25,11 +25,11 @@ class SignupScreen extends StatelessWidget {
           Positioned(
               top: 0,
               right: 0,
-              child: Image.asset('assets/icons/upperTyre.png')),
+              child: Image.asset('assets/icons/upper.png',height: 230)),
           Positioned(
               bottom: 0,
               left: 0,
-              child: Image.asset('assets/icons/lowerTyre.png')),
+              child: Image.asset('assets/icons/lower.png',height: 230)),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Center(
