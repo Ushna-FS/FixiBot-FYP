f215b9b1f1b6f5eb4d768ef88cdd33669de0cec0
MominaAmir <mominaamir145@gmail.com>
2025-09-26 00:49:41 +0500

    added vehicles brand and model list to make the dropdowns

---
+++ lib/screens/vehicle/model/vehiclebrandModel.dart
@@ -0,0 +1,81 @@
+// lib/data/brand_models.dart
+const List<Map<String, dynamic>> carBrandData = [
+  {
+    "brand": "Toyota",
+    "models": [
+      "Yaris","Corolla","Camry","Corolla Cross","Fortuner",
+      "Fortuner GR-S","Hilux","Land Cruiser Prado","Land Cruiser 300","Rush"
+    ]
+  },
+  {
+    "brand": "Honda",
+    "models": ["City","Civic","BR-V","HR-V"]
+  },
+  {
+    "brand": "Suzuki",
+    "models": ["Alto","Wagon R","Swift","Ciaz","Bolan","Every","Jimny"]
+  },
+  {
+    "brand": "Hyundai",
+    "models": ["Elantra","Sonata","Tucson","Porter H-100"]
+  },
+  {
+    "brand": "Kia",
+    "models": ["Picanto","Sportage","Sorento","Stonic","Grand Carnival"]
+  },
+  {
+    "brand": "Changan",
+    "models": ["Alsvin","Oshan X7","Karvaan"]
+  },
+  {
+    "brand": "Haval",
+    "models": ["H6","Jolion"]
+  },
+  {
+    "brand": "MG",
+    "models": ["MG HS","MG ZS","MG ZS EV","MG Cyberster"]
+  },
+  {
+    "brand": "Proton",
+    "models": ["Saga","X70"]
+  },
+  {
+    "brand": "Chery",
+    "models": ["Tiggo 4 Pro","Tiggo 8 Pro"]
+  },
+  {
+    "brand": "Audi",
+    "models": [
+      "A3","A4","A5","A6","A7","A8","Q2","Q3","Q5","Q7","e-tron","e-tron GT","Q4 e-tron","R8","TT"
+    ]
+  },
+  {
+    "brand": "Peugeot",
+    "models": ["2008","5008"]
+  },
+  {
+    "brand": "Prince",
+    "models": ["Pearl"]
+  },
+  {
+    "brand": "United Auto Industries",
+    "models": ["Bravo"]
+  },
+  {
+    "brand": "FAW",
+    "models": ["V2","XPV","Carrier"]
+  }
+];
+
+
+
+    // Move the list inside build method since it's used in the UI
+    final List<String> vehicleTypes = [
+      'bike',
+      'car',
+      'truck',
+      'van',
+      'suv',
+      'bus',
+      'other'
+    ];
