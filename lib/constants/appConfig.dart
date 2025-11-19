cf719c77523de3238f9bc94101bb5d2c764d3ad0
Ushna-FS <ushnafaheem616@gmail.com>
2025-11-19 20:21:04 +0500

    Added  release signing configuration with key.properties(google authentication accross multiple devices)

--- lib/constants/appConfig.dart
+++ lib/constants/appConfig.dart
@@ -1,4 +1,5 @@
 class AppConfig {
-  
-  static const String baseUrl = "https://zoogloeal-byron-unruled.ngrok-free.dev";
+ 
+  // static const String baseUrl = "https://zoogloeal-byron-unruled.ngrok-free.dev";
+static const String baseUrl = "https://chalky-anjelica-bovinely.ngrok-free.dev";
 }
